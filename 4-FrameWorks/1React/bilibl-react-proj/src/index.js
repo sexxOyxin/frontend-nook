080f0ef50266ba71213028f42d594e2710017ad3
strongXin <2441319772@qq.com>
2025-04-14 17:11:37 +0800

    node in react

--- 4-FrameWorks/1React/bilibl-react-proj/src/index.js
+++ 4-FrameWorks/1React/bilibl-react-proj/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// console.log(__dirname, 'dirnameeee');
+// console.log(__filename, 'filenameee');
+console.log(process, 'processss');
+
 root.render(
     <App />
 );
